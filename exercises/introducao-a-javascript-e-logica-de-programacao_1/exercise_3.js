85903d12f098915201af8a09eb730e8c8629a115
Tiago Ponce <tiagocponce@gmail.com>
2021-06-23 18:58:58 -0300

    Compara 3 numeros e retorna o maior valor

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise_3.js
@@ -0,0 +1,11 @@
+let num1 = 10;
+let num2 = 20;
+let num3 = 30;
+
+if (num1 > num2 && num1 > num3) {
+    return num1;
+}   else if (num2 > num1 && num2 > num3) {
+    return num2;
+} else {
+    return num3;
+}
