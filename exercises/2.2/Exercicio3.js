2b638ff130ba63c934ddad902c69f6b6870892c6
Tiago Ponce <tiagocponce@gmail.com>
2021-06-24 15:42:58 -0300

    Exercicio 3 e 4

---
+++ exercises/2.2/Exercicio3.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let mediaAritimetica = 0;
+
+for (let i = 0 ; i < numbers.length - 1; i++) {
+    mediaAritimetica = numbers[i] + mediaAritimetica;
+}
+
+mediaAritimetica = mediaAritimetica/numbers.length;
+console.log(mediaAritimetica);
