17d6cea3fccbdba8ba0f30111b3d03b7a3b50bf4
Tiago Ponce <tiagocponce@gmail.com>
2021-07-22 15:04:12 -0300

    Exercicio 2 - sort array

---
+++ exercises/Fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/Exercicio02.js
@@ -0,0 +1,12 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+
+
+oddsAndEvens.sort((a,b) => {
+  if (a > b) return 1;
+  if (a < b) return -1;
+  return 0;
+});
+
+console.log(oddsAndEvens);
