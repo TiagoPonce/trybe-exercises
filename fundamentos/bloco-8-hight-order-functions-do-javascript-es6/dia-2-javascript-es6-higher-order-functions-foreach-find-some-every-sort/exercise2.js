0f4d99881c43b3cd1c5ae6effd3f5e873d4e35da
Tiago Ponce <tiagocponce@gmail.com>
2021-08-03 18:09:08 -0300

    exercicios

---
+++ fundamentos/bloco-8-hight-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise2.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function getNamedBook() {
+  return expectedResult.find((book) => book.name.length === 26);
+}
+
+assert.deepStrictEqual(getNamedBook(), expectedResult);
